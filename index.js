43c262f8885ed8d5232ac0489108814f88f3515d
Jeremiah <jeremiahmarcos27@outlook.com>
2021-10-13 10:03:06 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+const saturdayFun = (activity = 'roller-skate') => {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = (activity = "go to the office") => {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (wrap = "*") => {
+    return function innerFunction(additionalWrap = "awesome") {
+         return`You are ${wrap}${additionalWrap}${wrap}!`
+    }
+}
+wrapAdjective()
